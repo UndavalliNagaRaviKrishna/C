a094c5c66b6d5f5620dc8a17d154dafbf37a48f6
Undavalli Naga Ravi Krishna <ravikrishna2349@gmail.com>
2026-02-08 22:59:50 +0530

    Create Patterns/stars.c

---
+++ Patterns/5*5.c
@@ -0,0 +1,21 @@
+// Logic to print star based on user required rows and columns
+
+#include <stdio.h>
+
+int main()
+{
+  int row=0, col=0;
+  printf("Enter the number of rows:");
+  scanf("%d",&row);
+  printf("Enter the number of columns:");
+  scanf("%d",&col);
+  for(int i=0; i<row;i++)
+  {
+     for(int j=0; j<col; j++)
+     {
+        printf("* ");
+     }
+        printf("\n");
+  }
+}
+        

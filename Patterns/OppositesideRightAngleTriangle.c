7875e8c0c607bcbbac8035a0148937c2b1068304
Undavalli Naga Ravi Krishna <ravikrishna2349@gmail.com>
2026-02-18 11:31:46 +0530

    Refactor triangle printing logic and remove column input
    Updated the triangle printing logic to remove column input and adjust loop conditions.

--- Patterns/OppositesideRightAngleTriangle.c
+++ Patterns/OppositesideRightAngleTriangle.c
@@ -1,18 +1,24 @@
 // Logic to print oppsite side right angle triangle
+/* OUTPUT: for 6 rows          
+             *
+           * *
+         * * *
+       * * * *
+     * * * * *
+   * * * * * * 
+  */
 
 #include <stdio.h>
 
 int main()
 {
-  int row=0, col=0;
+  int row=0;
   printf("Enter the number of Rows:");
   scanf("%d",&row);
-  printf("Enter the number of Col:");
-  scanf("%d",&col);
   
-  for(int i=0;i<row;i++)
+  for(int i=0;i<=row;i++)
   {
-     for(int j=col;j>i;j--)
+     for(int j=0;j<row-i;j++)
      {
         printf("  ");
      }
